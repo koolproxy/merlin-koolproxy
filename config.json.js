e5fa49ba722e2775516a31480909e412f42c90a0
lixin <sadoneli@gmail.com>
2016-12-27 10:12:00 +0800

    first commit of koolproxy

---
+++ config.json.js
@@ -0,0 +1,9 @@
+{
+    "build_date": "2016-12-27_09:15:58", 
+    "description": "3.0.0版本代码重构,性能更强,拒绝bug~", 
+    "home_url": "Module_koolproxy.asp", 
+    "md5": "86e3f562e60338eda5a19e9345d48dd4", 
+    "module": "koolproxy", 
+    "title": "koolproxy", 
+    "version": "3.0.0"
+}
