5b3caa26c3608e52b5fc9c01b84c7b49a6476ec8
lixin <sadoneli@gmail.com>
2017-01-06 19:40:36 +0800

    koolproxy 3.0.3

--- config.json.js
+++ config.json.js
@@ -2,8 +2,8 @@
     "build_date": "2016-12-27_09:15:58", 
     "description": "3.0.0版本代码重构,性能更强,拒绝bug~", 
     "home_url": "Module_koolproxy.asp", 
-    "md5": "e1f78fb7aef3c1aa2c7c5220c0a10774", 
+    "md5": "9ee27432a2148e081dd31deb89a59a44", 
     "module": "koolproxy", 
     "title": "koolproxy", 
-    "version": "3.0.2"
+    "version": "3.0.3"
 }
