7700af2973865356f20752353c829e6750e66301
lixin <sadoneli@gmail.com>
2017-01-08 15:54:02 +0800

    update koolproxy to 3.0.4#2

--- config.json.js
+++ config.json.js
@@ -1,6 +1,6 @@
 {
     "build_date": "2016-12-27_09:15:58", 
-    "description": "3.0.0版本代码重构,性能更强,拒绝bug~", 
+    "description": "3.0.4修复淘宝等IOS应用的自定义协议透传问题", 
     "home_url": "Module_koolproxy.asp", 
     "md5": "48ad4c50afce5bede6297d9b7f7d7a68", 
     "module": "koolproxy", 
