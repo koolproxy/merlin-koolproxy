8d6bdb819535d810c16046ca22297acc37374797
lixin <sadoneli@gmail.com>
2017-01-24 21:58:54 +0800

    update koolproxy to 3.2.1#2

--- config.json.js
+++ config.json.js
@@ -2,7 +2,7 @@
     "build_date": "2016-12-27_09:15:58", 
     "description": "3.1.0性能优化，添加https支持！", 
     "home_url": "Module_koolproxy.asp", 
-    "md5": "ee125b9690e0d12c8c8419991079d617", 
+    "md5": "6d5aaa67bc06eab7e9443d43bb4b44ed", 
     "module": "koolproxy", 
     "title": "koolproxy", 
     "version": "3.2.1"
--- koolproxy/scripts/koolproxy_config.sh
+++ koolproxy/scripts/koolproxy_config.sh
@@ -3,9 +3,9 @@ eval `dbus export koolproxy`
 
 
 if [ "$koolproxy_enable" == "1" ];then
-	sh /koolshare/koolproxy/koolproxy.sh restart  > /tmp/koolproxy_run.log
+	sh /koolshare/koolproxy/kp_config.sh restart  > /tmp/koolproxy_run.log
 else
-	sh /koolshare/koolproxy/koolproxy.sh stop  > /tmp/koolproxy_run.log
+	sh /koolshare/koolproxy/kp_config.sh stop  > /tmp/koolproxy_run.log
 fi
 echo XU6J03M6 >> /tmp/koolproxy_run.log
 sleep 1
