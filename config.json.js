573d3b0c43bdcafba9156851ad81658a85920370
lixin <sadoneli@gmail.com>
2017-01-24 22:07:15 +0800

    update koolproxy to 3.2.1#4

--- config.json.js
+++ config.json.js
@@ -2,7 +2,7 @@
     "build_date": "2016-12-27_09:15:58", 
     "description": "3.1.0性能优化，添加https支持！", 
     "home_url": "Module_koolproxy.asp", 
-    "md5": "73baca6fcd0a1c1e695887ff27ab3070", 
+    "md5": "dc56e54933b0b3e838f746520427ac05", 
     "module": "koolproxy", 
     "title": "koolproxy", 
     "version": "3.2.1"
