13f5dc144d830f731258efc0ac2f0920af7cb0cb
lixin <sadoneli@gmail.com>
2017-01-25 20:15:19 +0800

    sync latest rules

--- config.json.js
+++ config.json.js
@@ -2,7 +2,7 @@
     "build_date": "2016-12-27_09:15:58", 
     "description": "3.1.0性能优化，添加https支持！", 
     "home_url": "Module_koolproxy.asp", 
-    "md5": "dff84d9e6e591dae2652521ddea9bf2b", 
+    "md5": "6dbaed288bf3706290aa1fda0dd3d15f", 
     "module": "koolproxy", 
     "title": "koolproxy", 
     "version": "3.2.2.1"
