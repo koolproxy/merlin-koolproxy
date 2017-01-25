c6618a74e720ee9cb1bdbe659dcba94fd62994a2
lixin <sadoneli@gmail.com>
2017-01-25 20:18:54 +0800

    change description

--- config.json.js
+++ config.json.js
@@ -1,6 +1,6 @@
 {
     "build_date": "2016-12-27_09:15:58", 
-    "description": "3.1.0性能优化，添加https支持！", 
+    "description": "koolproxy, your right choice!", 
     "home_url": "Module_koolproxy.asp", 
     "md5": "6dbaed288bf3706290aa1fda0dd3d15f", 
     "module": "koolproxy", 
