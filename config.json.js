163d882ceba4baecf82a8f5ea38fc785dcd7e9bf
lixin <sadoneli@gmail.com>
2017-02-03 16:36:04 +0800

    fix user.txt show in web

--- config.json.js
+++ config.json.js
@@ -2,8 +2,8 @@
     "build_date": "2016-12-27_09:15:58", 
     "description": "祝大家春节快乐！", 
     "home_url": "Module_koolproxy.asp", 
-    "md5": "d690c8cb08c237f75ebf87c91eba3ac5", 
+    "md5": "46fcf9cb6ae59726e6ecb9dcc05882c2", 
     "module": "koolproxy", 
     "title": "koolproxy", 
-    "version": "3.2.4.1"
+    "version": "3.2.4.2"
 }
