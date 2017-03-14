899de8b7d042bd0d2fdf801fc505058bae546dba
lixin <sadoneli@gmail.com>
2017-03-14 23:49:19 +0800

    update kp

--- config.json.js
+++ config.json.js
@@ -2,7 +2,7 @@
     "build_date": "2016-12-27_09:15:58", 
     "description": "祝大家春节快乐！", 
     "home_url": "Module_koolproxy.asp", 
-    "md5": "4223f339a2e74315fd172bc508dfc91c", 
+    "md5": "44cf62a7cc4b843342324901a87f8a13", 
     "module": "koolproxy", 
     "title": "koolproxy", 
     "version": "3.3.4"
