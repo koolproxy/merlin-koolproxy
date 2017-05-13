d12ccacb6822807e3cdfd71b967da147ee2533d8
sadoneli <sadoneli@gmail.com>
2017-05-13 20:45:53 +0800

    Update config.json.js

--- config.json.js
+++ config.json.js
@@ -1,6 +1,6 @@
 {
     "build_date": "2016-12-27_09:15:58", 
-    "description": "祝大家春节快乐！", 
+    "description": "去广告，没烦恼", 
     "home_url": "Module_koolproxy.asp", 
     "md5": "b3be517417a5db31aad8d330137c691d", 
     "module": "koolproxy", 
