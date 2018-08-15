9a6a4bc251973c04e5a7605d6c65f194c835f880
sadoneli <sadoneli@gmail.com>
2018-08-15 10:29:39 +0800

    update 3.8.1

--- config.json.js
+++ config.json.js
@@ -2,8 +2,8 @@
     "build_date": "2016-12-27_09:15:58", 
     "description": "去广告，没烦恼", 
     "home_url": "Module_koolproxy.asp", 
-    "md5": "c2defe71ae9790f3841e848e24843adc", 
+    "md5": "bb5b3ff6e3c7f6814ea542e92476c550", 
     "module": "koolproxy", 
     "title": "koolproxy", 
-    "version": "3.7.2.1"
+    "version": "3.8.1.1"
 }
