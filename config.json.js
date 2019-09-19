75c68a9b63eb5b97b3c897cbf2c3e53509ded7ec
sadoneli <sadoneli@gmail.com>
2019-09-19 11:57:45 +0800

    fix offline version

--- config.json.js
+++ config.json.js
@@ -2,7 +2,7 @@
     "build_date": "2016-12-27_09:15:58", 
     "description": "去广告，没烦恼", 
     "home_url": "Module_koolproxy.asp", 
-    "md5": "f9026f6504226aa3846e6fa74fdf6c49", 
+    "md5": "f2f763ebbc9fc75b4e4b56ba49930ac6", 
     "module": "koolproxy", 
     "title": "koolproxy", 
     "version": "3.8.4.1"
